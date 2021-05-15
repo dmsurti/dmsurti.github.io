1c026a15afea2e520f709928a0fdf63b0e918925
Deepak Surti <dmsurti@gmail.com>
2021-05-15 22:30:52 +0100

    Add line-numbers to code blocks.

--- js/custom_01.js
+++ js/custom_01.js
@@ -1,3 +1,7 @@
 const yourCustomFunction01 = (message) => {
   console.log(message);
 };
+
+document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+    codeBlock.classList.add('line-numbers');
+});
