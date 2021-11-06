bd1cfcc9bd003c6f0c729ef162a3bcb2b8753de6
Deepak Surti <dmsurti@gmail.com>
2021-11-06 21:24:49 +0000

    Modify CSS to make main content left aligned.
    Other css changes for listing, website main description, image.

--- js/custom_01.js
+++ js/custom_01.js
@@ -1,11 +1,3 @@
 const yourCustomFunction01 = (message) => {
   console.log(message);
 };
-
-document.querySelectorAll('pre > code').forEach(function (codeBlock) {
-    codeBlock.classList.add('line-numbers');
-});
-
-document.querySelectorAll('img').forEach(function (codeBlock) {
-    mediumZoom(codeBlock);
-});
